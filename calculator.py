f16fb3c8d8853c01509a2b4c823eb6a5b08fa7ac
Vijay Singh <vijay.rhce@gmail.com>
2025-02-26 23:26:34 +0530

    commit from freature branch

--- calculator.py
+++ calculator.py
@@ -12,6 +12,9 @@ class Calculator:
 
     def divide(self, a, b):
         return a / b
+    
+    def square_root(self, x):
+        return math.sqrt(x)
 
 if __name__ == "__main__":
     calculator = Calculator()
@@ -24,4 +27,6 @@ if __name__ == "__main__":
     print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
     print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
 
+    num3 = 25
+    print(f"The square root of {num3} = {calculator.square_root(num3)}")
 
