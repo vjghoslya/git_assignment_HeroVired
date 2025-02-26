7e8044e692681242ed3e76966f180e7bbe97cfd3
Vijay Singh <vijay.rhce@gmail.com>
2025-02-26 23:28:46 +0530

    error fixed devide by 0 bug

--- calculator.py
+++ calculator.py
@@ -11,6 +11,8 @@ class Calculator:
         return a * b
 
     def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero.")
         return a / b
 
 if __name__ == "__main__":
